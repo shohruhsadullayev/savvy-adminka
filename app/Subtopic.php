6809f7d0ffcba8b7606d7d8c889a335445b175da
Shohruh <49054875+shohruhsadullayev@users.noreply.github.com>
2020-12-25 09:32:00 +0500

    first commit

---
+++ app/Subtopic.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subtopic extends Model
+{
+    public $timestamps = false;
+
+}
---
+++ app/TestQuestion.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TestQuestion extends Model
+{
+    public $timestamps = false;
+
+}
